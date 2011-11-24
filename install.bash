2c36e664c4dd1762139205d27e0d5d8a013c3c64
David Peter <david.a.peter@gmail.com>
2011-11-23 18:26:44 -0800

    Install pathogen in install.bash

--- install.bash
+++ install.bash
@@ -1,3 +1,9 @@
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -so ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+
+# symlink dotfiles
 for dotfile in tmux.conf vimrc bash_profile gitconfig
 do
   echo $dotfile
