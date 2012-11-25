b99f619795f367408020a25b191641811126177a
David Peter <david@1000memories.com>
2012-11-25 18:13:46 -0500

    Make swaps directory for vim

--- install.bash
+++ install.bash
@@ -3,6 +3,9 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
+# make swaps directory for vim
+mkdir ~/.vim/swaps
+
 # symlink dotfiles
 for dotfile in tmux.conf vimrc bash_profile gitconfig
 do
