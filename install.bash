59a2410fa1176019c1a25c648fdba22c3513da66
David Peter <david@1000memories.com>
2012-12-09 21:50:38 -0500

    Cleaner install.

--- install.bash
+++ install.bash
@@ -3,8 +3,15 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
-# make swaps directory for vim
+# install nvm
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# make various directories for vim
 mkdir ~/.vim/swaps
+mkdir ~/.vim/backups
+mkdir ~/.vim/undo
+mkdir ~/.vim/colors
+curl -so ~/.vim/colors/molokai.vim https://raw.github.com/tomasr/molokai/master/colors/molokai.vim
 
 # symlink dotfiles
 for dotfile in tmux.conf vimrc bash_profile gitconfig git-completion.bash git-prompt.sh
