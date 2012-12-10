cc61ee0f8e411fa0316d497164342d9953f15ab5
David Peter <david@1000memories.com>
2012-12-09 21:54:20 -0500

    Add git-completion and git-prompt

--- install.bash
+++ install.bash
@@ -7,7 +7,7 @@ curl -so ~/.vim/autoload/pathogen.vim \
 mkdir ~/.vim/swaps
 
 # symlink dotfiles
-for dotfile in tmux.conf vimrc bash_profile gitconfig
+for dotfile in tmux.conf vimrc bash_profile gitconfig git-completion.bash git-prompt.sh
 do
   echo $dotfile
   ln -s `pwd`/$dotfile ~/.$dotfile
